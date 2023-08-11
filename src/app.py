60324d27ff645b987eaec141900d4cfed0c0622f
Samuel Herrera <futbol.ingeniero@gmail.com>
2023-08-11 14:02:30 +0000

    Se crearon los 3 entpoint correspondientes para el api de TODOS

---
+++ src/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+todos = [
+    {"label": "My first task", "done": False},
+]
+
+# GET TODOS
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    text_json = jsonify(todos)
+    return text_json
+
+# POST TODOS
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    todos.append(request_body)
+    text_json = jsonify(todos)
+    return text_json
+
+# DELETE TODOS
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    del todos[position]
+    text_json = jsonify(todos)
+    return text_json 
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
